771e20b00ad19ee88eca1f00fad291e3caf184fb
Sadiashraful-KCL <sadiashraful18@gmail.com>
2019-04-05 16:33:55 +0100

    Final Update of the IOS application 'CalorieTracker.ai'

---
+++ CalorieTracker.ai/CalorieTracker.ai/View/OnboardingView.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingView.swift
+//  CalorieTracker.ai
+//
+//  Created by Sadi Ashraful on 26/11/2018.
+//  Copyright © 2018 Sadi Ashraful. All rights reserved.
+//
+
+import UIKit
+import paper_onboarding
+
+class OnboardingView: PaperOnboarding {
+
+}
